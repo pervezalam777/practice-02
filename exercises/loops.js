ff0b4e857d54ce76c96e473c0582a9ba5aceeedb
Pervez alam <pervezalam777@gmail.com>
2022-09-26 20:38:51 +0530

    Session 14 loop practice

---
+++ exercises/loops.js
@@ -0,0 +1,33 @@
+// console.log('*');
+// console.log('* *');
+// console.log('* * *');
+// console.log('* * * *');
+// console.log('* * * * *');
+
+let outerIndex = 0;
+while(outerIndex < 5){
+    let str = '';
+    let innerIndex = 0;
+    while(innerIndex <= outerIndex) {
+        str += '* '
+        innerIndex += 1;
+    }
+    console.log(str);
+    outerIndex += 1;
+}
+
+// console.log('    *')
+// console.log('   **')
+// console.log('  ***')
+// console.log(' ****')
+// console.log('*****')
+
+// Code
+
+// console.log('    *    ');
+// console.log('   * *   ');
+// console.log('  * * *  ');
+// console.log(' * * * * ');
+// console.log('* * * * *');
+
+// Code
--- variables/array.js
+++ variables/array.js
@@ -218,5 +218,4 @@ const passportList = [
 
 
 
-
 // map
