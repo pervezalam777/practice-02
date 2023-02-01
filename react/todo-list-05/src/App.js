acb29d8ef41af1fb35ad1a568906c6956e4db084
Pervez alam <pervezalam777@gmail.com>
2023-02-01 20:31:22 +0530

    todo component separation

---
+++ react/todo-list-05/src/App.js
@@ -0,0 +1,14 @@
+//import TodoComponent from './components/todoComponent';
+import TodoContainerComponent from './components/todoContainerComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <TodoComponent /> */}
+     <TodoContainerComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/todo-list-05/src/components/todoListComponent.js
@@ -0,0 +1,9 @@
+
+function TodoListComponent(props) {
+  const { todos } = props;
+  return (
+    <ul>list....</ul>
+  )
+}
+
+export default TodoListComponent;
