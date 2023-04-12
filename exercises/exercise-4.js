e280069d47d31d6b3ae27d03dae77e2dae8c0f27
Pervez alam <pervezalam777@gmail.com>
2023-04-12 20:07:27 +0530

    exercise 4 movie object

---
+++ exercises/exercise-4.js
@@ -0,0 +1,15 @@
+const movie = {
+  name:"sholey",
+  releaseDate: '1980',
+  cast: [
+    { actor: 'Amitabh'},
+    { actor: 'Sanjeev'},
+    { actor: 'Dharmender'},
+    { actor: 'Hema'},
+    { actor: 'Jaya'}
+  ],
+  cost: '100000',
+  earning: '10000000'
+}
+
+const updatedMovie = {...movie};
